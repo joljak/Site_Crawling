dd3872210996d570f252ecdbe130a7834c3ec14c
jeje <las9897@gmail.com>
2019-03-05 15:24:33 +0900

    modified clien_crawling.py: divide function

--- clien_crawling.py
+++ clien_crawling.py
@@ -4,7 +4,7 @@ group = ['community', 'allinfo', 'allreview', 'allsell']
 link_list = []
 session = HTMLSession(mock_browser=True)
 
-def crawling(url:str):
+def post_crawling(url:str):
     
     url = "https://www.clien.net" + url
     r = session.get(url)
@@ -15,18 +15,17 @@ def crawling(url:str):
         if i.text != "":
             print(i.text)
 
-    r = session.get(url+"/comment?ps=1000")
-    r.html.render(wait=3)
+def comment_crawling(url:str):
+    url = "https://www.clien.net" + url
+    print(url)
+    r = session.get(url)
+    r.html.render(wait=10)
     comment_view = r.html.find('.comment_view', first=False)
-    print("comment")
-    #print(comment_view)  # element
     for comment in comment_view:
         ps = comment.find('p')
         for p in ps:
             if p.text != "":
                 print(p.text)
-
-
 
 def getLink(group: str, start_page:int, end_page: int):
     for page in range(start_page, end_page):
@@ -34,8 +33,9 @@ def getLink(group: str, start_page:int, end_page: int):
         list_subject = r.html.find('.symph_row > .list_title > .list_subject')
         for subject in list_subject:
             link_list.append(subject.attrs['href'])
+
     for link in link_list:
-        crawling(link)
-        
-getLink(group[0], 0, 1)
+        comment_crawling(link)
+        #post_crawling(link)
+getLink(group[0], 7, 8)
 
