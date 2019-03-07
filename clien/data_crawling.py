dd921ac3dff189ba21d07621142dc4633f94f939
jeje <las9897@gmail.com>
2019-03-07 09:43:35 +0900

    clien complete

--- clien/data_crawling.py
+++ clien/data_crawling.py
@@ -1,46 +1,77 @@
+import csv
+import datetime
+import os
+import time
+
 from requests_html import HTMLSession
 from lxml.etree import ParserError
 
+FILE_DIRECTORY = os.path.abspath(os.path.join(__file__, "../datafile"))
+TODAY_DATE = datetime.date.today().isoformat()
+file_name = FILE_DIRECTORY + f'/{TODAY_DATE}_Clien_Data.csv'
 group = ['community', 'allinfo', 'allreview', 'allsell']
 link_list = []
 session = HTMLSession(mock_browser=True)
 
-def post_crawling(url:str):
-    url = "https://www.clien.net" + url
-    r = session.get(url)
-    post_content = r.html.find('.post_content', first=True)
-    post_p = post_content.find('p', first=False)
-    print(url)
+
+def data_crawling(url: str):
+    # POST
+    r = session.get("https://www.clien.net" + url)
+    post_nickname = r.html.find('.nickname', first=True)
+    img = post_nickname.find('img', first=True)
+    if img is not None:
+        nickname = img.attrs['alt']
+    else:
+        nickname = post_nickname.text
+    print("https://www.clien.net" + url)
+    post_p = r.html.find('.post_content', first=True).find('p')
     for i in post_p:
         if i.text != "":
-            print(i.text)
+            with open(file_name, 'a', encoding='utf-8', newline='\n') as link_csv:
+                csv.writer(link_csv).writerow(["https://www.clien.net" + url, "post", nickname, i.text])
 
-def comment_crawling(url:str):
-    url = "https://www.clien.net" + url.split("?")[0] + "/comment?ps=1000"
-    print(url)
-    r = session.get(url)
-  
+    # COMMENT
+    r = session.get("https://www.clien.net" + url.split("?")[0] + "/comment?ps=1000")
+
     try:
-        comment_view = r.html.find('.comment_view', first=False)
+        comment_row = r.html.find('.comment_row')
     except ParserError:
-        print("Not exist comment!")
         return
-    for comment in comment_view:
-        ps = comment.find('p')
-        for p in ps:
-            if p.text != "":
-                print(p.text)
 
-def getLink(group: str, start_page:int, end_page: int):
+    for comment in comment_row:
+        # except '삭제되었습니다.'
+        if comment.find('.blocked', first=True) is not None:
+            continue
+        nickname = comment.find('.nickname', first=True)
+        img = nickname.find('img', first=True)
+        if img is not None:
+            nickname = img.attrs['alt']
+        else:
+            nickname = nickname.text
+
+        comment_view = comment.find('.comment_view')
+        for comment in comment_view:
+            ps = comment.find('p')
+            for p in ps:
+                if p.text != "":
+                    with open(file_name, 'a', encoding='utf-8', newline='\n') as link_csv:
+                        csv.writer(link_csv, dialect='excel').writerow(
+                            ["https://www.clien.net" + url, "comment", nickname, p.text])
+
+
+def getLink(group: str, start_page: int, end_page: int):
     for page in range(start_page, end_page):
-        r = session.get('https://www.clien.net/service/group/'+ group + "?po="+ str(page))
+        r = session.get('https://www.clien.net/service/group/' + group + "?po=" + str(page))
         list_subject = r.html.find('.symph_row > .list_title > .list_subject')
         for subject in list_subject:
             link_list.append(subject.attrs['href'])
 
     for link in link_list:
-        comment_crawling(link)
-        #post_crawling(link)
-getLink(group[0], 0, 1)
+        data_crawling(link)
+    time.sleep(3)
 
 
+for group in group:
+    getLink(group, 0, 10)
+    getLink(group, 11, 20)
+    getLink(group, 21, 30)
