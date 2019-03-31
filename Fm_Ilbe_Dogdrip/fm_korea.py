d5eb66daab3124fdd3a4a2adcaeb6c2abe913913
roamgom <roamgom@gmail.com>
2019-04-01 00:36:19 +0900

    edit fm_korea content crawl code

--- Fm_Ilbe_Dogdrip/fm_korea.py
+++ Fm_Ilbe_Dogdrip/fm_korea.py
@@ -16,13 +16,13 @@ from s3_bucket_manage import upload_s3
 FILE_DIRECTORY = os.path.abspath(os.path.join(__file__, "../.."))
 
 # S3 bucket config
-OBJ_FOLDER = "FM_Korea"
+OBJ_FOLDER = "FM_Ilbe_Dogdrip"
 with open(os.path.join('bucket_name.json')) as slang_file:
     S3_BUCKET = json.load(slang_file)['bucket']
 s3 = boto3.client('s3')
 
 
-def collect_fm_korea_document_link(keyword):
+def collect_fm_korea_document_link(keyword, start_page, end_page):
     """
     FM 코리아 통합검색 링크 크롤링
     :param keyword: 욕설키워드
@@ -33,35 +33,37 @@ def collect_fm_korea_document_link(keyword):
     session = HTMLSession(mock_browser=True)
 
     # Check page for each keyword
-    pages_text = session.get(
-        f'https://www.fmkorea.com/index.php?act=IS&is_keyword={keyword}&mid=home&where=document&page=0').html.find(
-        '#content > div > h3 > span', first=True)
-    result_pages = int(re.sub("[^0-9]", "", pages_text.text))
-    pages = result_pages if result_pages < 1000 else 1000
+    # pages_text = session.get(
+    #     f'https://www.fmkorea.com/index.php?act=IS&is_keyword={keyword}&mid=home&where=document&page=0').html.find(
+    #     '#content > div > h3 > span', first=True)
+    # result_pages = int(re.sub("[^0-9]", "", pages_text.text))
+    # pages = result_pages if result_pages < 1000 else 1000
 
-    print(f'Crawling page: {pages}')
+    print(f'Crawling page: {start_page} to {end_page}')
 
     bot.sendMessage(chat_id=CHAT_ID,
-                    text=f'Keyword: {keyword} Crawling page: {pages}')
+                    text=f'Keyword: {keyword} Crawling page: {start_page} to {end_page}')
 
     # Close session for list pages
     session.close()
 
     # Make csv file to save document link
-    file_name = f'fm_korea/FM_korea_{keyword}_links.csv'
+    file_name = f'fm_korea/FM_korea_{keyword}_{start_page}-{end_page}_links.csv'
 
     os.makedirs(os.path.dirname(file_name), exist_ok=True)
     if os.path.exists(file_name) is False:
         open(file_name, 'a').close()
 
-    bar = Bar('Processing', max=pages)
-    for number in range(pages):
+    session = HTMLSession(mock_browser=True)
+    bar = Bar('Processing', max=end_page-start_page)
+
+    for number in range(start_page, end_page):
         try:
             # Search link and text result via keyword
             time.sleep(12)
             bar.next()
 
-            session = HTMLSession(mock_browser=True)
+            fake_session_page = session.get('https://www.fmkorea.com/')
 
             fm_korea_docs = session.get(
                 f'https://www.fmkorea.com/index.php?act=IS&is_keyword={keyword}&mid=home&where=document&page={number + 1}').html
@@ -99,20 +101,20 @@ def collect_fm_korea_document_link(keyword):
             bot.sendMessage(chat_id=CHAT_ID,
                             text=str(e))
     bot.sendMessage(chat_id=CHAT_ID,
-                    text=f'FM_Korea {content_type} {keyword}({slang_choice}) link Done!\n')
+                    text=f'FM_Korea {content_type} {keyword}({slang_choice}) {start_page}-{end_page} link Done!\n')
     bar.finish()
     upload_s3(s3, S3_BUCKET, file_name, '/'.join([OBJ_FOLDER, file_name]))
     session.close()
 
 
-def collect_fm_korea_document_content(keyword):
+def collect_fm_korea_document_content(keyword, start_page, end_page):
     """
     FM 코리아 통합검색 내용 크롤링
     :param keyword: 욕설키워드
     :return:
     """
 
-    link_file_name = f'fm_korea/FM_korea_{keyword}_links.csv'
+    link_file_name = f'fm_korea/FM_korea_{keyword}_{start_page}-{end_page}_links.csv'
     content_file_name = f'fm_korea/FM_korea_{keyword}_contents.csv'
     if os.path.exists(link_file_name) is False:
         # Check CSV file exists
@@ -134,14 +136,16 @@ def collect_fm_korea_document_content(keyword):
             content_writer.writeheader()
             content_csv.close()
 
+        # Make a new session for crawling
+        session = HTMLSession(mock_browser=True)
+
+        bar = Bar('Processing', max=end_page - start_page)
+
         for line in line_reader:
             # Crawl content data from each link_line
             try:
+                bar.next()
                 link = ''.join(line)
-                print(f'get {link}')
-
-                # Make a new session for crawling
-                session = HTMLSession(mock_browser=True)
 
                 page_result = session.get(link).html
 
@@ -168,12 +172,13 @@ def collect_fm_korea_document_content(keyword):
                         # If the content is not blank
                         bot.sendMessage(chat_id=CHAT_ID,
                                         text=f'link_id: {link[24:]} body empty')
+                        continue
                     else:
                         for body in body_divide:
                             if body == '':
                                 continue
                             else:
-                                content_writer.writerow({'link': link, 'content': body.text.replace("\n", "")})
+                                content_writer.writerow({'link': link, 'content': body.replace("\n", "")})
 
                     # Comment text
                     comments = page_result.find('ul.fdb_lst_ul > li > div:nth-of-type(2) > div.xe_content')
@@ -186,10 +191,12 @@ def collect_fm_korea_document_content(keyword):
                         comment_content = comment.text.replace("\n", " ")
                         if comment_content == '':
                             continue
+                        elif '[삭제된 댓글입니다.]' in comment_content:
+                            continue
                         else:
                             # If the content is not blank
                             content_writer.writerow({'link': link, 'content': comment_content})
-                    time.sleep(13)
+                    time.sleep(14)
 
                 # Close session
                 session.close()
@@ -197,6 +204,7 @@ def collect_fm_korea_document_content(keyword):
             except Exception as e:
                 bot.sendMessage(chat_id=CHAT_ID,
                                 text=str(e))
+        bar.finish()
 
     bot.sendMessage(chat_id=CHAT_ID,
                     text=f'FM_Korea {content_type} {keyword}({slang_choice}) content Done!\n')
@@ -207,7 +215,9 @@ if __name__ == '__main__':
     if len(sys.argv) < 2:
         # Exit if number of argument is incorrect
         print('가져올 데이터 [link, content]')
+        print('비속어 선정 [slang, unordered]')
         print('단어 선택 unordered: [0 - 180] | namu_wiki: [0-156]\n')
+        print('페이지 ')
         print('usage: python fm_korea.py link namu_wiki 0')
         exit()
 
@@ -218,6 +228,9 @@ if __name__ == '__main__':
     # TODO: namu_wiki에서 애매한 비속어 제거 후 업데이트
     # 단어 선택 unordered: [0 - 180] | namu_wiki: [0-156]
     slang_choice = int(sys.argv[3])
+
+    start_page = int(sys.argv[4])
+    end_page = int(sys.argv[5])
 
     with open(os.path.join(FILE_DIRECTORY, 'slang.json')) as slang_file:
         # Open slang.json to read slang words
@@ -230,10 +243,10 @@ if __name__ == '__main__':
 
     print(f'FM_Korea {content_type} Crawling start!!\n')
     bot.sendMessage(chat_id=CHAT_ID,
-                    text=f'FM_Korea {content_type} {keyword}({slang_choice}) Crawling start!!\n')
+                    text=f'FM_Korea {content_type} {keyword}({slang_choice}) {start_page}-{end_page} Crawling start!!\n')
 
     if content_type == 'link':
-        collect_fm_korea_document_link(keyword)
+        collect_fm_korea_document_link(keyword, start_page, end_page)
 
     elif content_type == 'content':
-        collect_fm_korea_document_content(keyword)
+        collect_fm_korea_document_content(keyword, start_page, end_page)
