6104df30fcf981d8a89ec60ecb822da3f6ef3c15
sunimooni <jspark7373@naver.com>
2019-04-01 09:35:51 -0700

    저장 포맷 변경, s3 변경

--- bbombu/crawl_bbombu.py
+++ bbombu/crawl_bbombu.py
@@ -15,108 +15,109 @@ S3 = ""
 
 # UPLOAD AWS_S3 - BOTO3
 def upload(s3, local_file_path, bucket, obj):
-	s3.upload_file(local_file_path, bucket, obj)
+    s3.upload_file(local_file_path, bucket, obj)
 
 # [S]KEYWORD SEARCH AND COLLECT LINK
 def collect_document_link(keyword, pages):
 	
-	collect_link_num = 0
+    collect_link_num = 0
 	
-	# HTMLSession with mock_browser 
-	session = HTMLSession(mock_browser=True)
+    # HTMLSession with mock_browser 
+    session = HTMLSession(mock_browser=True)
 
-	# CREATE LINKS FILE
-	links_file = FILE_DIRECTORY + f'/bbombu_{keyword}_links.csv'	
-	if os.path.exists(links_file):
-		os.remove(links_file)
-	open(links_file,'a').close()
+    # CREATE LINKS FILE
+    links_file = FILE_DIRECTORY + f'/bbombu_{keyword}_links.csv'	
+    if os.path.exists(links_file):
+        os.remove(links_file)
+    open(links_file,'a').close()
 
-	# Progressing Bar	
-	bar = Bar(f'Processing : Link Crawling [{keyword}]', max = pages)
+    # Progressing Bar	
+    bar = Bar(f'Processing : Link Crawling [{keyword}]', max = pages)
 
-	# Crawl Link
-	for i in range(pages):
-		time.sleep(5)
-		bar.next()
-		r = session.get(f'http://www.ppomppu.co.kr/search_bbs.php?page_no={i + 1}&keyword={keyword}')
-		dummy_links = r.html.find('#result-tab1 > form > div > div > span > a')
-		with open(links_file,'a',newline='') as link:
-			wr = csv.writer(link)
-			for i in dummy_links:
-				wr.writerow([i.attrs['href']])
-				collect_link_num += 1
+    # Crawl Link
+    for i in range(pages):
+        time.sleep(3)
+        bar.next()
+        r = session.get(f'http://www.ppomppu.co.kr/search_bbs.php?page_no={i + 1}&keyword={keyword}')
+        dummy_links = r.html.find('#result-tab1 > form > div > div > span > a')
+        with open(links_file,'a',newline='') as link:
+            wr = csv.writer(link)
+            for i in dummy_links:
+                wr.writerow([i.attrs['href']])
+                collect_link_num += 1
 
-	# Close
-	bar.finish()
-	session.close()
+    # Close
+    bar.finish()
+    session.close()
 	
-	return collect_link_num
+    return collect_link_num
 # [E]KEYWORD SEARCH AND COLLECT LINK
 
 
 # [S]COLLECT DOCUMENT CONTENT BY LINK
 def collect_document_content(keyword, num):
 	
-	# HTMLSession with mock browser
-	session = HTMLSession(mock_browser=True)
+    # HTMLSession with mock browser
+    session = HTMLSession(mock_browser=True)
 
-	# CREATE CONTENTS FILE
-	contents_file = FILE_DIRECTORY + f'/bbombu_{keyword}_contents.csv'
-	if os.path.exists(contents_file):
-		os.remove(contents_file)
-	content = open(contents_file,'a', encoding = 'utf-8')
-	wr_contents = csv.writer(content)
+    # CREATE CONTENTS FILE
+    contents_file = FILE_DIRECTORY + f'/bbombu_{keyword}_contents.csv'
+    if os.path.exists(contents_file):
+        os.remove(contents_file)
+    content = open(contents_file,'a', encoding = 'utf-8')
+    wr_contents = csv.writer(content)
 	
-	# OPEN LINKS FILE
-	links_file = FILE_DIRECTORY + f'/bbombu_{keyword}_links.csv'
-	if os.path.exists(links_file) is False:
-		print("Link file is not exist\n")
-		exit()	
-	link = open(links_file,'r',newline='')
-	rd_links = csv.reader(link)
+    # OPEN LINKS FILE
+    links_file = FILE_DIRECTORY + f'/bbombu_{keyword}_links.csv'
+    if os.path.exists(links_file) is False:
+        print("Link file is not exist\n")
+        exit()	
+    link = open(links_file,'r',newline='')
+    rd_links = csv.reader(link)
 	
-	# Progressing Bar
-	bar = Bar(f'Processing : Content Crawling [{keyword}]', max =num)
+    # Progressing Bar
+    bar = Bar(f'Processing : Content Crawling [{keyword}]', max =num)
 
-	# Crawl Contents
-	for rd_link in rd_links:
-		store = []
-		time.sleep(5)
-		r = session.get(rd_link[0])
+    # Crawl Contents
+    for rd_link in rd_links:
+        time.sleep(3)
+        r = session.get(rd_link[0])
 		
-		# error handler
-		if(r.html.find('.error1',first=True)) is not None:
-			return
-		
-		title = r.html.find('.view_title2',first=True)
-		post = r.html.find('.ori_comment')
-		for i in post:
-			store.append(i.text.replace("\n"," "))
-		wr_contents.writerow([rd_link[0],title.text]+store)
-		bar.next()
-		exit()	
+        # error handler
+        if(r.html.find('.error1',first=True)) is not None:
+            return
 
-	# UPLOAD TO S3
-	upload(S3,contents_file,BUCKET,"BBOMBU"+f"/bbombu_{keyword}_contents.csv")
+        title = r.html.find('.view_title2',first=True)
+        wr_contents.writerow([title.text])
 
-	# Close
-	bar.finish()
-	content.close()
-	link.close()	
-	session.close()	
-	os.remove(links_file)
+        post = r.html.find('.ori_comment')
+        for i in post:
+            if(i.text != ""):
+                wr_contents.writerow([i.text])
+        bar.next()
+	    	
+
+    # UPLOAD TO S3
+#    upload(S3,contents_file,BUCKET,"BBOMBU"+f"/bbombu_{keyword}_contents.csv")
+
+    # Close
+    bar.finish()
+    content.close()
+    link.close()	
+    session.close()	
+    os.remove(links_file)
 # [E]COLLECT DOCUMENT BY LINK
 
 if __name__ == '__main__':
 
-	S3 = boto3.client('s3')
+    S3 = boto3.client('s3')
 
-	with open(SLANG_FILE) as slang_file:
-		slangs = json.load(slang_file)["unordered"]
+    with open(SLANG_FILE) as slang_file:
+        slangs = json.load(slang_file)["unordered"]
 	
-	for slang in slangs:
-		num = collect_document_link(slang,4)
-		collect_document_content(slang,num)
+    for slang in slangs:
+        num = collect_document_link(slang,100)
+        collect_document_content(slang,num)
 	
 
 
--- dc_inside/crawl_DCinside.py
+++ dc_inside/crawl_DCinside.py
@@ -15,114 +15,110 @@ S3 = ""
 
 # UPLOAD AWS_S3 - BOTO3
 def upload(s3, local_file_path, bucket, obj):
-	s3.upload_file(local_file_path, bucket, obj)
+    s3.upload_file(local_file_path, bucket, obj)
 
 
 # [S]KEYWORD SEARCH AND COLLECT LINK
 def collect_document_link(keyword, pages):
 	
-	collect_link_num = 0
+    collect_link_num = 0
 	
-	# HTMLSession with mock_browser 
-	session = HTMLSession(mock_browser=True)
+    # HTMLSession with mock_browser 
+    session = HTMLSession(mock_browser=True)
 
-	# CREATE LINKS FILE
-	links_file = FILE_DIRECTORY + f'/dc_inside_{keyword}_links.csv'	
-	if os.path.exists(links_file):
-		os.remove(links_file)
-	open(links_file,'a').close()
+    # CREATE LINKS FILE
+    links_file = FILE_DIRECTORY + f'/dc_inside_{keyword}_links.csv'	
+    if os.path.exists(links_file):
+    	os.remove(links_file)
+    open(links_file,'a').close()
 
-	# Progressing Bar	
-	bar = Bar(f'Processing : Link Crawling [{keyword}]', max = pages)
+    # Progressing Bar	
+    bar = Bar(f'Processing : Link Crawling [{keyword}]', max = pages)
 
-	# Crawl Link
-	for i in range(pages):
-		time.sleep(5)
-		bar.next()
-		r = session.get(f'https://search.dcinside.com/post/p/{i + 1}/sort/latest/q/{keyword}')
-		dummy_links = r.html.find('#container > div > section.center_content > div.inner > div.integrate_cont.sch_result.result_all > ul > li > a')
-		with open(links_file,'a', encoding='utf-8',newline='') as link:
-			wr = csv.writer(link)
-			for i in dummy_links:
-				wr.writerow([i.attrs['href']])
-				collect_link_num += 1
+    # Crawl Link
+    for i in range(pages):
+    	time.sleep(3)
+    	bar.next()
+    	r = session.get(f'https://search.dcinside.com/post/p/{i + 1}/sort/latest/q/{keyword}')
+    	dummy_links = r.html.find('#container > div > section.center_content > div.inner > div.integrate_cont.sch_result.result_all > ul > li > a')
+    	with open(links_file,'a', encoding='utf-8',newline='') as link:
+    	    wr = csv.writer(link)
+    	    for i in dummy_links:
+                wr.writerow([i.attrs['href']])
+                collect_link_num += 1
 
-	# Close
-	bar.finish()
-	session.close()
+    # Close
+    bar.finish()
+    session.close()
 	
-	return collect_link_num
+    return collect_link_num
 # [E]KEYWORD SEARCH AND COLLECT LINK
 
 
 # [S]COLLECT DOCUMENT CONTENT BY LINK
 def collect_document_content(keyword, num):
 		
-	# HTMLSession with mock browser
-	session = HTMLSession(mock_browser=True)
+    # HTMLSession with mock browser
+    session = HTMLSession(mock_browser=True)
 
-	# CREATE CONTENTS FILE
-	contents_file = FILE_DIRECTORY + f'/dc_inside_{keyword}_contents.csv'
-	if os.path.exists(contents_file):
-		os.remove(contents_file)
-	content = open(contents_file,'a', encoding = 'utf-8')
-	wr_contents = csv.writer(content)
+    # CREATE CONTENTS FILE
+    contents_file = FILE_DIRECTORY + f'/dc_inside_{keyword}_contents.csv'
+    if os.path.exists(contents_file):
+        os.remove(contents_file)
+    content = open(contents_file,'a', encoding = 'utf-8')
+    wr_contents = csv.writer(content)
 	
-	# OPEN LINKS FILE
-	links_file = FILE_DIRECTORY + f'/dc_inside_{keyword}_links.csv'
-	if os.path.exists(links_file) is False:
-		print("Link file is not exist\n")
-		exit()	
-	link = open(links_file,'r',newline='')
-	rd_links = csv.reader(link)
+    # OPEN LINKS FILE
+    links_file = FILE_DIRECTORY + f'/dc_inside_{keyword}_links.csv'
+    if os.path.exists(links_file) is False:
+        print("Link file is not exist\n")
+        exit()	
+    link = open(links_file,'r',newline='')
+    rd_links = csv.reader(link)
 	
-	# Progressing Bar
-	bar = Bar(f'Processing : Content Crawling [{keyword}]', max =num)
+    # Progressing Bar
+    bar = Bar(f'Processing : Content Crawling [{keyword}]', max =num)
 
-	# Crawl Contents
-	for rd_link in rd_links:
-		store = []
-		time.sleep(5)
-		r = session.get(rd_link[0])
-		# error handler
-		if(r.html.find('.box_infotxt delet',first=True)) is not None:
-			return
+    # Crawl Contents
+    for rd_link in rd_links:
+        time.sleep(3)
+        r = session.get(rd_link[0])
+        # error handler
+        if(r.html.find('.box_infotxt delet',first=True)) is not None:
+            return
+        
+        title = r.html.find('#container > section > article:nth-child(3) > div.view_content_wrap > header > div > h3 > span.title_subject')
+        wr_contents.writerow([title[0].text])
+        
+        post = r.html.find('.writing_view_box',first =True)
+        for i in ['p']:
+            posts = post.find(i)
+            for p in posts:
+                if(p.text != ""):
+                    wr_contents.writerow([p.text])	
+        bar.next()	
 		
-		title = r.html.find('#container > section > article:nth-child(3) > div.view_content_wrap > header > div > h3 > span.title_subject')
-		post = r.html.find('.writing_view_box',first =True)
-		for i in ['div','p','span']:
-			posts = post.find(i)
-			for p in posts:
-				store.append(p.text.replace("\n"," "))
-		wr_contents.writerow([rd_link[0],title[0].text]+store)	
-		bar.next()	
-		
-	# UPLOAD TO S3
-	upload(S3,contents_file,BUCKET,"DC_INSIDE"+f"/dc_inside_{keyword}_contents.csv")
+#    # UPLOAD TO S3
+#    upload(S3,contents_file,BUCKET,"DC_INSIDE"+f"/dc_inside_{keyword}_contents.csv")
 
-	# Close
-	bar.finish()
-	content.close()
-	link.close()	
-	session.close()	
-	os.remove(links_file)
+    # Close
+    bar.finish()
+    content.close()
+    link.close()	
+    session.close()	
+    os.remove(links_file)
 # [E]COLLECT DOCUMENT BY LINK
 
 
 if __name__ == '__main__':
 	
-	S3 = boto3.client('s3')
+    S3 = boto3.client('s3')
 	
-	with open(SLANG_FILE) as slang_file:
-		slangs = json.load(slang_file)["unordered"]
+    with open(SLANG_FILE) as slang_file:
+        slangs = json.load(slang_file)["unordered"]
 	
-	for slang in slangs:
-		num = collect_document_link(slang,4)
-		collect_document_content(slang,num)
+    for slang in slangs:
+        num = collect_document_link(slang,100)
+        collect_document_content(slang,num)
 	
-
-
-
-
-
 
