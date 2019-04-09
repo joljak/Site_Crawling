0adb45e40e182a2e832d2733562c21ea714e375f
jeje <las9897@gmail.com>
2019-04-09 06:16:14 +0000

    modified s3_bucket_manage

--- s3_bucket_manage.py
+++ s3_bucket_manage.py
@@ -14,16 +14,27 @@ if __name__ == '__main__':
     with open(os.path.join(ROOT_DIRECTORY, 'slang.json'), encoding='utf-8') as slang_file:
         SLANG = json.load(slang_file)['unordered']
     s3 = boto3.client('s3')
-    S3_BUCKET = "dankook-hunminjeongeum-data-bucket-test"
+    S3_BUCKET = "dankook-hunminjeongeum-data-bucket"
+
+    if len(sys.argv) < 4:
+        exit('''
+                Argument Error
+                Choice Command [upload, download]
+                Choice Site [Clien, Inven, Ruliweb]
+                Choice Type [content, processed, labeled]
+                usage) s3_bucket_manage.py [Command] [Site] [Type]
+                ''')
 
     command = sys.argv[1] if sys.argv[1] in ['upload', 'download'] else exit('Choice [upload, download')
-    site = sys.argv[2] if sys.argv[2] in ['Inven', 'Clien', 'Ruliweb'] else exit('Site Error')
+    site = sys.argv[2] if sys.argv[2] in ['Inven', 'Clien', 'Ruliweb'] else exit('Choice [Inven, Clien, Ruliweb]')
+    type = sys.argv[3] if sys.argv[3] in ['content', 'processed', 'labeled'] else exit(
+        'Choice [content, processed, labeled')
 
     for keyword in SLANG:
-        file = f'{site}_{keyword}_content.csv'
+        file = f'{site}_{keyword}_{type}.csv'
         local_path = os.path.join(os.path.join(ROOT_DIRECTORY, site), file)
         if os.path.exists(local_path) is False:
             continue
 
         if command == 'upload':
-            upload_s3(local_path, '/'.join([site, file]))
+            upload_s3(local_path, '/'.join([site, type, file]))
